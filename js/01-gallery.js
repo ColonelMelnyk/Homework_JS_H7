68d95be3b5262fc63f3979bf3c220b4d3b628623
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-04-22 00:23:30 +0300

    task 1 pt 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createImgGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+function createImgGallery(galleryItems){
+return galleryItems.map(({preview, original, description}) =>{
+    return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+}).join(``);
 
-console.log(galleryItems);
+}
+gallery.addEventListener("click", magnification);
+function magnification(){
+
+}
